f5e971c4b94af071ad93da6afeab3bfa8e869d41
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-13 11:11:20 +0700

    feat: add modal window

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="app">
-    <post-form
-      @create="createPost"
-    />
+    <h1>Страница с постами</h1>
+    <my-button
+      @click="showDialog"
+      style="margin: 15px 0 50px;"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+        @create="createPost"
+      />
+    </my-dialog>
     <post-list
       :posts="posts"
       @remove="removePost"
@@ -24,16 +33,21 @@ export default {
         {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
         {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
       ],
+      dialogVisible: false,
     }
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       // старый массив заменяется новым из постов id которых не равен тому который мы удаляем
       this.posts = this.posts.filter(p => p.id !== post.id)
-    }
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
   }
 }
 </script>
