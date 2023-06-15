daa6ac633f6902900bf6ded308aa560cb7102d32
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-15 21:42:39 +0700

    feat: add paginated navigation

---
+++ src/pages/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Добро пожаловать!</h1>
+    <!-- <router-link to="/posts">Посты</router-link>
+    <router-link to="/about">О сайте</router-link> -->
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
