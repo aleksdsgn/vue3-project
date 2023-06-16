2d8474e1d1d98f502bac9897c044d8d7e8924df2
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-06-16 11:20:50 +0700

    feat: add global state examples

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import components from '@/components/UI';
 import router from '@/router/router';
-import VIntersection from '@/directives/VIntersection';
 import directives from '@/directives';
+import store from '@/store';
 
 const app = createApp(App);
 
@@ -18,4 +18,5 @@ directives.forEach(directive => {
 
 app
   .use(router)
+  .use(store)
   .mount('#app');
